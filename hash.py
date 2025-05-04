9846b32cfdd44419d504c4168c8dee16bf019172
Andrew-Atwater <andrew.atwater@maine.edu>
2025-05-04 17:14:12 -0400

    working

--- hash.py
+++ hash.py
@@ -5,3 +5,9 @@ def hash(k, m = 1000):
     frac = (k * A) % 1
     return (m * frac)
 
+val_arr = [71, 72, 73, 74, 75]
+hashing = {k: hash(k) for k in val_arr}
+
+for k in hashing:
+    print(f"h({k}) = {hash(k)}")
+
